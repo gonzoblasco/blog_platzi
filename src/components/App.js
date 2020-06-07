ff78bfb9f4e561be70a79ac4159b9f0d7e3d29ab
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-07 15:17:52 -0300

    Prueba Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src='' className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Correo</th>
+        <th>Enlace</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Rodolfo</td>
+        <td>gonzoblasco@gmail.com</td>
+        <td>gonzaloblasco.com</td>
+      </tr>
+      <tr>
+        <td>Platzi</td>
+        <td>platzi@gmail.com</td>
+        <td>platzi.com</td>
+      </tr>
+      </tbody>
+    </table>
   );
 }
-
-export default App;
