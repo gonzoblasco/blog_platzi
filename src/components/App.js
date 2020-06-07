ca1140d689f94d20a8e7c92cc654e13ed67a4d53
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-07 16:00:23 -0300

    Ciclo de vida de React

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      usuarios: [],
+    };
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(respuesta.data);
+
+    this.setState({
       usuarios: [
         {
           nombre: 'Gonzalo Blasco',
@@ -16,12 +26,12 @@ class App extends Component {
           enlace: 'platzi.com',
         },
       ],
-    };
+    });
   }
 
   ponerFilas = () => (
     this.state.usuarios.map(usuario => (
-      <tr>
+      <tr key={ usuario.correo }>
         <td>{ usuario.nombre }</td>
         <td>{ usuario.correo }</td>
         <td>{ usuario.enlace }</td>
