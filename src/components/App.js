2b9749738f2b3cd267d7260c94c405778e1eced9
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-07 15:27:18 -0300

    Agregando funciones a la app con React

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 
+const ponerFilas = () => [
+  <tr>
+    <td>Gonzalo Blasco</td>
+    <td>gonzoblasco@gmail.com</td>
+    <td>gonzaloblasco.com</td>
+  </tr>,
+  <tr>
+    <td>Platzi</td>
+    <td>platzi@gmail.com</td>
+    <td>platzi.com</td>
+  </tr>,
+];
+
+
 export default () => {
   return (
-    <table>
-      <thead>
-      <tr>
-        <th>Nombre</th>
-        <th>Correo</th>
-        <th>Enlace</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>Rodolfo</td>
-        <td>gonzoblasco@gmail.com</td>
-        <td>gonzaloblasco.com</td>
-      </tr>
-      <tr>
-        <td>Platzi</td>
-        <td>platzi@gmail.com</td>
-        <td>platzi.com</td>
-      </tr>
-      </tbody>
-    </table>
+    <div className='margen'>
+      <table className='tabla'>
+        <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Correo</th>
+          <th>Enlace</th>
+        </tr>
+        </thead>
+        <tbody>
+        { ponerFilas() }
+        </tbody>
+      </table>
+    </div>
   );
 }
