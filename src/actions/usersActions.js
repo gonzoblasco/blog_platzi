1decc1d1e3bca9656195d0c1e098c2881ac92bd6
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-08 18:44:31 -0300

    Try Catch

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 import { GET_ALL_USERS } from '../types/usersTypes';
 
 export const getAllUsers = () => async (dispatch) => {
-  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
 
-  dispatch({
-    type: GET_ALL_USERS,
-    payload: response.data,
-  });
+    dispatch({
+      type: GET_ALL_USERS,
+      payload: response.data,
+    });
+  } catch ( e ) {
+    console.error('Error:', e.message);
+  }
 };
