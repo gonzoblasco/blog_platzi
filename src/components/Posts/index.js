535ee40634594fa8c1ae09ec6f0bfe66634ad90f
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-09 17:12:42 -0300

    Compartir Reducer

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Posts extends Component {
+import * as usersActions from '../../actions/usersActions';
+
+class Posts extends Component {
+  componentDidMount() {
+    if ( !this.props.users.length ) {
+      this.props.getAllUsers();
+    }
+  }
+
   render() {
+    const user = this.props.users;
+    console.log(user);
     return (
       <div>
+        <h1>Posts by </h1>
         { this.props.match.params.key }
       </div>
     );
   }
 }
+
+const mapStateToProps = (reducers) => reducers.usersReducer;
+
+export default connect(mapStateToProps, usersActions)(Posts);
