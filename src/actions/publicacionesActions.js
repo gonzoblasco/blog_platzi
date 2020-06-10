0799f157a2998084085c6e934f01eb809a02a600
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-10 18:45:40 -0300

    Validación de errores

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -5,31 +5,43 @@ import * as usuariosTypes from '../types/usuariosTypes';
 const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
+  dispatch({
+    type: CARGANDO,
+  });
   const { publicaciones } = getState().publicacionesReducer;
   const { usuarios } = getState().usuariosReducer;
   const usuario_id = usuarios[key].id;
 
-  const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${ usuario_id }`);
+  try {
+    const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${ usuario_id }`);
 
-  const publicaciones_actualizadas = [
-    ...publicaciones,
-    respuesta.data,
-  ];
+    const publicaciones_actualizadas = [
+      ...publicaciones,
+      respuesta.data,
+    ];
 
-  const publicaciones_key = publicaciones_actualizadas.length - 1;
-  const usuarios_actualizados = [ ...usuarios ];
-  usuarios_actualizados[key] = {
-    ...usuarios[key],
-    publicaciones_key,
-  };
+    dispatch({
+      type: TRAER_POR_USUARIO,
+      payload: publicaciones_actualizadas,
+    });
 
-  dispatch({
-    type: USUARIOS_TRAER_TODOS,
-    payload: usuarios_actualizados,
-  });
+    const publicaciones_key = publicaciones_actualizadas.length - 1;
+    const usuarios_actualizados = [ ...usuarios ];
+    usuarios_actualizados[key] = {
+      ...usuarios[key],
+      publicaciones_key,
+    };
 
-  dispatch({
-    type: TRAER_POR_USUARIO,
-    payload: publicaciones_actualizadas,
-  });
+    dispatch({
+      type: USUARIOS_TRAER_TODOS,
+      payload: usuarios_actualizados,
+    });
+  } catch ( error ) {
+    console.log(error.message);
+
+    dispatch({
+      type: ERROR,
+      payload: 'Publicaciones no disponibles.',
+    });
+  }
 };
