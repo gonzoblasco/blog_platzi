9b9f1a04f399c38323cdb8f610e2737c9998cbd3
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-10 22:55:55 -0300

    Introduccion a Metodos HTTP / Nuevo Ciclo Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './Menu';
+import Tareas from './Tareas';
 import Usuarios from './Usuarios';
 import Publicaciones from './Publicaciones';
 
-const Prueba = () => <div>hola</div>;
-
-const App = (props) => (
+const App = () => (
   <BrowserRouter>
     <Menu />
     <div id="margen">
       <Route exact path='/' component={ Usuarios } />
-      <Route exact path='/tareas' component={ Prueba } />
+      <Route exact path='/tareas' component={ Tareas } />
       <Route exact path='/publicaciones/:key' component={ Publicaciones } />
     </div>
   </BrowserRouter>
